5edbbd6dca3c4757c3277051c993caa7e01dabeb
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-06-04 13:54:12 +0100

    create search bar component

--- instagram-app/src/components/SearchBar/SearchBar.js
+++ instagram-app/src/components/SearchBar/SearchBar.js
@@ -1,24 +1,25 @@
-import React, { Component } from 'react'
+import React from "react";
+import "./SearchBar.css";
 
-export class SearchBar extends Component {
-    render() {
-        return (
-            <div>
-                <header>
-                    <div>
-                        <div class="logo"></div> 
-                        <div class="logo-name"></div>
-                    </div>
-                    <div class="search"></div>
-                    <div>
-                        <div id="navigation"></div>
-                        <div id="cupid"></div>
-                        <div id="user"></div>
-                    </div>
-                </header>
-            </div>
-        )
-    }
+export default function SearchBar() {
+  return (
+    <div className="searchBar">
+      <div className="logo">
+        <i className="fab fa-instagram fa-3x" />
+        <hr />
+        <p>Instagram</p>
+      </div>
+
+      <div className="searchInput">
+        <i className="fas fa-search" />
+        <input placeholder="Search" />
+      </div>
+
+      <div className="searchContent">
+        <i className="far fa-compass fa-2x" />
+        <i className="far fa-heart fa-2x" />
+        <i className="far fa-user fa-2x" />
+      </div>
+    </div>
+  );
 }
-
-export default SearchBar
