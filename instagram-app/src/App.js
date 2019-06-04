0f56085a21b8d2e5393bc00e0620cc9f6b505338
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-06-04 13:56:13 +0100

    fix error in components and remove redundant code

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -2,7 +2,9 @@ import React, { Component } from 'react'
 // import logo from './logo.svg';
 import './App.css';
 import dummyData from './dummy-data';
-import PostContainer from './components/PostContainer/PostContainer'
+import PostContainer from './components/PostContainer/PostContainer';
+import SearchBar from "./components/SearchBar/SearchBar";
+
 
 
 export class App extends Component {
@@ -14,13 +16,20 @@ export class App extends Component {
   }
   render() {
     return (
-      dummyData.map((data) =>{
+      <div>
+        <SearchBar />
+
+        {
+          dummyData.map((data) =>{
         return (
           <PostContainer 
             dummyData={data}
           />
         )
       })
+        }
+      </div>
+      
     )
   }
 }
