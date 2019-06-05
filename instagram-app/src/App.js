2fb8c2b080454ad88e8da81a7dc5039a85a7fd2f
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-06-05 13:32:24 +0100

    flesh out app.js with day two instructions and learning using, to handel search and likes

--- instagram-app/src/App.js
+++ instagram-app/src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import dummyData from './dummy-data';
 import PostContainer from './components/PostContainer/PostContainer';
 import SearchBar from "./components/SearchBar/SearchBar";
+import { Triple } from "react-preloading-component";
+
 
 
 
@@ -47,23 +49,64 @@ export class App extends Component {
       return this.setState({ data });
     }
   };
-  render() {
-    return (
-      <div>
-        <SearchBar />
 
-        {
-          dummyData.map((data) =>{
-        return (
-          <PostContainer key={data.username} 
-            data={data}
-          />
-        )
-      })
+  handleClick = (e, index) => {
+    if (e.key === "Enter") {
+      let newComment = {
+        username: "annonymous",
+        text: e.target.value
+      };
+      let { data } = this.state;
+      data[index].comments = data[index].comments.concat(newComment);
+      this.setPostData(data);
+      this.setState({ data });
+    }
+  };
+
+  handleSearch = e => {
+    this.setState({ data: this.getPostsData() });
+    let searchData = e.target.value;
+
+    if (searchData.length > 0) {
+      let newData = this.state.data.filter(post => {
+        if (post.username.includes(e.target.value)) {
+          return post;
         }
-      </div>
-      
-    )
+        return null;
+      });
+      console.log(newData);
+      this.setState({ data: newData });
+    }
+  };
+  
+  render() {
+    if (this.state.data.length > 0) {
+      return (
+        <div className="App">
+          <SearchBar handleSearch={this.handleSearch} />
+          {this.state.data.map((data, i) => {
+            return (
+              <PostContainer
+                key={data.username}
+                data={data}
+                handleLike={this.handleLike}
+                index={i}
+                setPostData={this.setPostData}
+                handleClick={this.handleClick}
+              />
+            );
+          })}
+        </div>
+      );
+    }
+    return (
+      <>
+        <SearchBar handleSearch={this.handleSearch} />
+        <div className="preloader">
+          <Triple color="#a7a9ac" size={80} />
+        </div>
+      </>
+    );
   }
 }
 
