98ab686038caba808ac8b4e9509eaf18e8fb1ed5
Anuoluwapo Apiti <anuoluwa.apiti@gmail.com>
2019-06-06 07:54:40 +0100

    add logic for login for users with new folder in a component

---
+++ instagram-app/src/components/Login/Login.js
@@ -0,0 +1,52 @@
+import React, { useState } from "react";
+import "./Login.css";
+
+export default function Login() {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const usernameHandler = e => setUsername(e.target.value);
+  const passwordHandler = e => setPassword(e.target.value);
+
+  const loginHandler = () => {
+    if (username !== "" && password !== "") {
+      localStorage.setItem("token", username);
+      window.location.reload();
+    }
+  };
+
+  return (
+    <>
+      <div className="login">
+        <h2>Instagram</h2>
+        <div className="login-form">
+          <form onSubmit={loginHandler}>
+            <input
+              onChange={usernameHandler}
+              type="text"
+              placeholder="Phone number, Username, or email "
+              required
+            />
+            <input
+              onChange={passwordHandler}
+              type="password"
+              placeholder="Password"
+              required
+            />
+            <button onClick={loginHandler}>Log In</button>
+          </form>
+
+          <h6>
+            <a href="#">Forgot Password?</a>
+          </h6>
+        </div>
+      </div>
+
+      <div className="signup">
+        <p>Don't have an account? </p>
+        <a href="#">Sign Up</a>
+      </div>
+
+    </>
+  );
+}
